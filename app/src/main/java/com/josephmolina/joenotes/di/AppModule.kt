bcef75023cee4f1b4e706de5cce10623703beccf
Joseph <josephmolina3388@gmail.com>
2022-01-17 12:22:33 -0800

    Setting up Dependency Injection with Hilt

---
+++ app/src/main/java/com/josephmolina/joenotes/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.josephmolina.joenotes.di
+
+import android.app.Application
+import androidx.room.Room
+import com.josephmolina.joenotes.feature_note.data.data_source.NoteDatabase
+import com.josephmolina.joenotes.feature_note.data.repository.NoteRepositoryImpl
+import com.josephmolina.joenotes.feature_note.domain.repository.NoteRepository
+import com.josephmolina.joenotes.feature_note.domain.use_case.DeleteNoteUseCase
+import com.josephmolina.joenotes.feature_note.domain.use_case.GetNotesUseCase
+import com.josephmolina.joenotes.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app, NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    /*
+    To test this we can create a testRepository and extend NoteRepository
+     */
+    @Provides
+    @Singleton
+    fun providesNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun providesNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(GetNotesUseCase(repository), DeleteNoteUseCase(repository))
+    }
+}
---
+++ app/src/main/java/com/josephmolina/joenotes/feature_note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,11 @@
+package com.josephmolina.joenotes.feature_note.domain.use_case
+/*
+Normally we would inject every usecase into a viewmodel but as the app
+grows we would have a huge constructor for the viewmodel.
+A wrapper class that we can use to call the other usecases related to this feature
+Inject single class that contains usecases into the viewmodel
+ */
+data class NoteUseCases(
+    val getNoteUseCase: GetNotesUseCase,
+    val deleteNoteUseCase: DeleteNoteUseCase
+)
